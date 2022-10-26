dec8812005e506df3fa5eb391767651c639ebad3
Marco Kadlubski <marco.kdlbsk@mailbox.org>
2022-10-26 18:02:50 +0200

    resolve merge conflict

--- src/Components/Main/Result/ResultComments.js
+++ src/Components/Main/Result/ResultComments.js
@@ -3,7 +3,18 @@ import React from "react";
 export default function ResultComments() {
   return (
     <div className="comments-container">
-      <svg></svg>
+      <svg
+        className="icon-comments"
+        fill="var(--color-secondary)"
+        viewBox="0 0 20 20"
+        xmlns="SVG namespace"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M18 13V5a2 2 0 00-2-2H4a2 2 0 00-2 2v8a2 2 0 002 2h3l3 3 3-3h3a2 2 0 002-2zM5 7a1 1 0 011-1h8a1 1 0 110 2H6a1 1 0 01-1-1zm1 3a1 1 0 100 2h3a1 1 0 100-2H6z"
+          clip-rule="evenodd"
+        ></path>
+      </svg>
       <a href="#" className="comments">
         3 Comments
       </a>
