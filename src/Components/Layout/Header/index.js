19e495fe93da01695f6826282a85fcb4053521a3
Christin Schade <christinabbe90gmail.com>
2022-10-28 16:17:50 +0200

    final

--- src/Components/Layout/Header/index.js
+++ src/Components/Layout/Header/index.js
@@ -1,10 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import "./header.css";
 import Logo from "./Logo";
 import Navbar from "./navigation/Navbar";
 import Searchbar from "../../Assets/Searchbar/Searchbar";
-import Searchbutton from "../../Assets/Searchbar/searchitems/Searchbutton";
-import Searchinput from "../../Assets/Searchbar/searchitems/Searchinput";
 
 export default function Header({ setSearchTerm }) {
   
